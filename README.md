f77b34b3f35e144a8fa0ca1226b1961162f60c93
Andres M. Romero <andresmromero7@gmail.com>
2024-09-07 05:58:19 -0500

    Add observability and monitoring tools
    - Integrate opentelemetry, loki, tempo, prometheus and grafana
    - Add observability and monitoring configuration to docker-compose
    - Include branch diagram

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # shop-spring-microservices-clean-arquitecture-ddd-cqrs
 ## 🔷  Project view
 <div align="center">
-<img src="./diagrams/1.initialize-user-context-and-infrastructure.gif" alt="Architecture Diagram" style="max-width: 800px; max-height: 400px; width: 100%; height: auto;">
+<img src="./diagrams/2.add-observability-and-monitoring.gif" alt="Architecture Diagram" style="max-width: 800px; max-height: 400px; width: 100%; height: auto;">
 </div>
+
+ - **Observability and monitoring:**  OpenTelemetry, Loki, Tempo, Prometheus, and Grafana
 
 ## 🔷 Modules
 <hr>
 
-- **User-context**
-<br>
-**TechStack:** Spring Boot
+- **User-context:** 
+
+    **Tech Stack:** Spring Boot
+<br><br>
--- user-context/src/main/java/com/amr/shop/usr/user_context/_shared/presentation/api/rest/UserStatusHealthCheckController.java
+++ user-context/src/main/java/com/amr/shop/usr/user_context/_shared/presentation/api/rest/UserStatusHealthCheckController.java
@@ -1,19 +1,23 @@
 package com.amr.shop.usr.user_context._shared.presentation.api.rest;
 
-import java.util.HashMap;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+
 @RestController
 @RequestMapping("/api/v1/health-check")
+@Slf4j
 public class UserStatusHealthCheckController {
 
     @GetMapping("bounded-contexts")
     public ResponseEntity<HashMap<String, String>> healthCheck() {
 
+        log.debug("user-context - status ok");
         HashMap<String, String> status = new HashMap<>();
         status.put("bounded_context", "user-context");
         status.put("status", "ok");
